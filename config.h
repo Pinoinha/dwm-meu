28aad31bf22956e0a758754eff5e2fb9c5c20c7b
Matheus R <matheusrbarros03@gmail.com>
2021-03-27 01:46:02 -0300

    Deletados arquivos .orig, e arrumadas configurações

--- config.h
+++ config.h
@@ -1,25 +1,25 @@
 /* See LICENSE file for copyright and license details. */
 
-/* Patch PulseAudio */
+/* Patches */
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 8;        /* gaps between windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;     /* 0 means no systray */
+static const int showsystray        = 0;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10", "fontawesome:size=12" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "Adobe Courier Regular:size=16", "Symbols Nerd Font:size=12" };
+static const char dmenufont[]       = "Adobe Courier Regular:size=14";
 static const char col_gray1[]       = "#121212";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#5c16ab";
+static const char col_cyan[]        = "#e60053";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -32,16 +32,25 @@ static const char *downvol[] = { "/usr/bin/pamixer", "--sink", "0", "-d", "5",  
 static const char *mutevol[] = { "/usr/bin/pamixer", "--sink",   "0", "-t",  NULL };
 
 /* tagging */
-static const char *tags[] = { "", "2", "3", "4", "5", "6", "7", "", "" };
+static const char *tags[] = { "", "", "", "切", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class      								instance    title       tags mask     isfloating   monitor */
+	{ "firefoxdeveloperedition",  NULL,       NULL,       1 << 4,       0,           -1 },
+	{ "LibreWolf",  							NULL,       NULL,       1 << 4,       0,           -1 },
+	{ "TelegramDesktop",  				NULL,       NULL,       1 << 3,       1,           -1 },
+	{ "lightcord",  							NULL,       NULL,       1 << 3,       0,      		 -1 },
+	{ "Steam",  									NULL,       NULL,       1 << 2,       1,      		 -1 },
+	{ "obs",  										NULL,       NULL,       1 << 2,       1,      		 -1 },
+	{ "Sxiv",  										NULL,       NULL,       0,       	1,      		 -1 },
+	{ "mpv",  										NULL,       NULL,       0,       	1,      		 -1 },
+	{ "Zathura",  								NULL,       NULL,       0,       	1,      		 -1 },
+	{ "Iwgtk",  									NULL,       NULL,       0,       	1,      		 -1 },
+	{ "Galculator",  							NULL,       NULL,       0,       	1,      		 -1 },
 };
 
 /* layout(s) */
@@ -59,6 +68,7 @@ static const Layout layouts[] = {
 /* key definitions */
 /* Tecla Meta = Mod4*/
 #define MODKEY Mod4Mask
+#define ALTKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -71,9 +81,9 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "urxvt", NULL };
-static const char *firefox[]  = { "firefox", NULL };
-static const char *xscreensvr[] = { "xscreensaver-command", "-lock", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
+static const char *firefox[]  = { "firefox-developer-edition", NULL };
+static const char *slock[] = { "slock", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -89,10 +99,10 @@ static Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY,             					XK_w,      killclient,     {0} },
+	{ MODKEY|ShiftMask,             XK_w,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY|ShiftMask,             XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY|ShiftMask,             XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,             					XK_f,      togglefullscr,  {0} },
@@ -100,10 +110,10 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
+	{ MODKEY,                       XK_bracketleft, shiftview,       {.i = -1 } },
+	{ MODKEY,                       XK_bracketright, shiftview,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY,                       XK_n,  		 shiftview,      {.i = -1 } },
-	{ MODKEY,                       XK_m,  		 shiftview,      {.i = +1 } },
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
@@ -116,11 +126,11 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ALTKEY,                XK_q,      quit,           {0} },
 	{ 0,                       XF86XK_AudioLowerVolume, spawn, {.v = downvol } },	/* Multimedia Keys Patch */
 	{ 0,                       XF86XK_AudioMute, 	    spawn, {.v = mutevol } },
 	{ 0,                       XF86XK_AudioRaiseVolume, spawn, {.v = upvol   } },
-	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v  = xscreensvr } }, // Tranca a tela com xscreensaver
+	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v  = slock } }, /* Tranca a tela com slock */
 };
 
 /* button definitions */
